522c22956ecad47649fede64b9b9bc2c621912b0
Jess Nunes <eng.diias@gmail.com>
2025-02-04 07:31:44 -0300

    Order

---
+++ src/main/java/com/devsuperior/dsecommerce/DsecommerceApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsecommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsecommerceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/devsuperior/dsecommerce/entities/OrderStatus.java
@@ -0,0 +1,6 @@
+package com.devsuperior.dsecommerce.entities;
+
+public enum OrderStatus {
+	
+	WAITINGPAYMENT,SHIPPED,DELIVERED,CANCELED;
+}
