bdf8dec1e5a20ad84a75d040091de18f928847a4
Jess Nunes <eng.diias@gmail.com>
2025-05-19 19:14:34 -0300

    Modification name method from class ResourceServeConfig

--- src/main/java/com/devsuperior/dscommerce/config/ResourceServerConfig.java
+++ src/main/java/com/devsuperior/dscommerce/config/ResourceServerConfig.java
@@ -79,7 +79,7 @@ public class ResourceServerConfig {
 	}
 
 	@Bean
-	FilterRegistrationBean<CorsFilter> corsFilter() {
+	FilterRegistrationBean<CorsFilter> FilterRegistrationBeanCorsFilter() {
 		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(
 				new CorsFilter(corsConfigurationSource()));
 		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
