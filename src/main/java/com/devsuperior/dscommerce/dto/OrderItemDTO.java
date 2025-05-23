9922fa26f9a9fbaf0dab32c74334910cd7444750
Jess Nunes <eng.diias@gmail.com>
2025-05-23 07:39:24 -0300

    Insert imgUrl in return of get orderItem

--- src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
@@ -7,16 +7,18 @@ public class OrderItemDTO {
 	private String name;
 	private Double price;
 	private Integer quantity;
+	private String imgUrl;
 
 	public OrderItemDTO() {
 		
 	}
-	public OrderItemDTO(Long productId, String name, Double price, Integer quantity) {
+	public OrderItemDTO(Long productId, String name, Double price, Integer quantity, String imgUrl) {
 
 		this.productId = productId;
 		this.name = name;
 		this.price = price;
 		this.quantity = quantity;
+		this.imgUrl = imgUrl;
 	}
 
 	public OrderItemDTO(OrderItem oi) {
@@ -24,6 +26,7 @@ public class OrderItemDTO {
 		name = oi.getProduct().getName();
 		price = oi.getPrice();
 		quantity = oi.getQuantity();
+		imgUrl = oi.getProduct().getImgUrl();
 	}
 
 	public Long getProductId() {
@@ -44,6 +47,10 @@ public class OrderItemDTO {
 
 	public Double getSubtotal() {
 		return price * quantity;
+	}
+	
+	public String getImgUrl() {
+		return imgUrl;
 	}
 
 }
